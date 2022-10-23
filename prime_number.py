4b250e9acba68bc0b0c76e43a06d869cd01d60bb
ParthKalkar <parthkalkar111@gmail.com>
2022-10-23 20:51:29 +0300

    prime_number

---
+++ prime_number.py
@@ -0,0 +1,17 @@
+import math
+from cmath import sqrt
+
+# brute force approach 
+def isPrime(n):
+    return False if n <=1 else all(n % i != 0 for i in range(2, n))
+
+
+# smart approach 
+def isPrimeSmart(n):
+    if n <= 1: 
+        return False
+    return all(n % i != 0 for i in range(2, math.ceil(math.sqrt(n)))) 
+
+
+print(isPrime(1))
+print(isPrimeSmart(2))
